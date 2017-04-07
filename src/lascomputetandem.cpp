72ff944a646ad99df40cc5414ee9dc93efc73c73
German Tischler <tischler@mpi-cbg.de>
2017-04-07 09:43:42 +0200

    add ground truth processing option in lascomputetandem

--- src/lascomputetandem.cpp
+++ src/lascomputetandem.cpp
@@ -20,11 +20,131 @@
 
 #include <libmaus2/dazzler/align/OverlapIndexer.hpp>
 #include <libmaus2/dazzler/align/AlignmentWriter.hpp>
+#include <libmaus2/dazzler/db/DatabaseFile.hpp>
 #include <libmaus2/util/ArgParser.hpp>
+#include <libmaus2/bambam/BamNumericalIndexDecoder.hpp>
+#include <libmaus2/bambam/BamNumericalIndexGenerator.hpp>
+#include <libmaus2/bambam/BamAccessor.hpp>
 
-int lascomputetandem(libmaus2::util::ArgParser const & arg, libmaus2::util::ArgInfo const & /* arginfo */)
+int64_t imax(int64_t const a, int64_t const b)
+{
+	return std::max(a,b);
+}
+
+void handle(
+	std::vector < libmaus2::math::IntegerInterval<int64_t> > & VI,
+	std::vector < libmaus2::math::IntegerInterval<int64_t> > & VT,
+	libmaus2::dazzler::db::DatabaseFile::unique_ptr_type & Pdb,
+	int64_t const prevaread,
+	int const verbose,
+	std::string const & bamfn
+)
+{
+	// if not empty
+	if ( VI.size() )
+	{
+		// merge intervals
+		VI = libmaus2::math::IntegerInterval<int64_t>::mergeTouchingOrOverlapping(VI);
+
+		// read length
+		int64_t const rlen =
+			VI.size() ?
+			(
+				Pdb ? Pdb->getRead(prevaread).rlen : -1
+			)
+			:
+			-1;
+
+		for ( uint64_t i = 0; i < VI.size(); ++i )
+		{
+			// read id
+			libmaus2::util::NumberSerialisation::serialiseNumber(std::cout,prevaread);
+			// interval start
+			libmaus2::util::NumberSerialisation::serialiseNumber(std::cout,VI[i].from);
+			// interval end
+			libmaus2::util::NumberSerialisation::serialiseNumber(std::cout,VI[i].to);
+
+			if ( verbose )
+			{
+				std::cerr << prevaread << "\t" << VI[i];
+				if ( rlen >= 0 )
+				{
+					// read length
+					std::cerr << "\t" << rlen;
+					// distance from start
+					std::cerr << "\t" << imax(0,VI[i].from);
+					// distance from end
+					std::cerr << "\t" << imax(0,rlen - VI[i].to);
+				}
+
+				if ( bamfn.size() )
+				{
+					// get BAM accessor
+					libmaus2::bambam::BamAccessor::unique_ptr_type tptr(new libmaus2::bambam::BamAccessor(bamfn,prevaread));
+					// get BAM record
+					libmaus2::bambam::BamAlignment const & algn = (*tptr)[prevaread];
+
+					// check size match
+					if ( rlen >= 0 )
+						assert ( algn.getLseq() == rlen );
+
+					// get reference interval for complete read
+					libmaus2::math::IntegerInterval<int64_t> intv = algn.getReferenceInterval();
+
+					// tandem interval on read
+					uint64_t ifrom = VI[i].from;
+					uint64_t ito = VI[i].to+1;
+
+					if ( algn.isReverse() )
+					{
+						std::swap(ifrom,ito);
+						ifrom = rlen - ifrom;
+						ito = rlen - ito;
+					}
+
+					// tandem interval on reference
+					uint64_t const tfrom = algn.getRefPosForReadPos(ifrom);
+					uint64_t const tto = algn.getRefPosForReadPos(ito-1);
+
+					libmaus2::math::IntegerInterval<int64_t> tintv(tfrom,tto);
+
+					std::cerr << "\t" << intv;
+					std::cerr << "\t" << tintv;
+
+					VT.push_back(tintv);
+				}
+
+				std::cerr << "\n";
+			}
+		}
+		VI.resize(0);
+	}
+
+}
+
+int lascomputetandem(libmaus2::util::ArgParser const & arg)
 {
 	bool const onlytrue = arg.uniqueArgPresent("onlytrue");
+	bool const verbose = arg.uniqueArgPresent("verbose");
+
+	std::string const db = arg.uniqueArgPresent("d") ? arg["d"] : std::string();
+	std::string const bamfn = arg.uniqueArgPresent("b") ? arg["b"] : std::string();
+
+	libmaus2::dazzler::db::DatabaseFile::unique_ptr_type Pdb;
+
+	if ( db.size() )
+	{
+		libmaus2::dazzler::db::DatabaseFile::unique_ptr_type Tdb(new libmaus2::dazzler::db::DatabaseFile(db));
+		Pdb = UNIQUE_PTR_MOVE(Tdb);
+	}
+
+	if ( bamfn.size() )
+	{
+		uint64_t const genindexmod = 128;
+		libmaus2::bambam::BamNumericalIndexGenerator::indexFileCheck(bamfn,genindexmod,1 /* numthreads */);
+	}
+
+	std::vector < libmaus2::math::IntegerInterval<int64_t> > VT;
 
 	for ( uint64_t i = 0; i < arg.size(); ++i )
 	{
@@ -37,32 +157,31 @@ int lascomputetandem(libmaus2::util::ArgParser const & arg, libmaus2::util::ArgI
 
 		while ( Plas->getNextOverlap(OVL) )
 		{
-			assert ( OVL.aread == OVL.bread );
+			// ignore non self overlaps
+			if ( OVL.aread != OVL.bread )
+				continue;
 
+			// new aread
 			if ( OVL.aread != prevaread )
-			{
-				VI = libmaus2::math::IntegerInterval<int64_t>::mergeTouchingOrOverlapping(VI);
-				for ( uint64_t i = 0; i < VI.size(); ++i )
-				{
-					libmaus2::util::NumberSerialisation::serialiseNumber(std::cout,prevaread);
-					libmaus2::util::NumberSerialisation::serialiseNumber(std::cout,VI[i].from);
-					libmaus2::util::NumberSerialisation::serialiseNumber(std::cout,VI[i].to);
-					// std::cerr << prevaread << "\t" << VI[i] << "\n";
-				}
-				VI.resize(0);
-			}
+				handle(VI,VT,Pdb,prevaread,verbose,bamfn);
 
+			// skip inverse overlaps
 			if ( OVL.isInverse() )
 				continue;
 
+			// get A and B intervals
 			libmaus2::math::IntegerInterval<int64_t> const IA(OVL.path.abpos,OVL.path.aepos-1);
 			libmaus2::math::IntegerInterval<int64_t> const IB(OVL.path.bbpos,OVL.path.bepos-1);
 
+			// if intersecting
 			if ( ! IA.intersection(IB).isEmpty() )
 			{
+				// compute union
 				libmaus2::math::IntegerInterval<int64_t> const IS = libmaus2::math::IntegerInterval<int64_t>::span(IA,IB);
+				// push union
 				VI.push_back(IS);
 			}
+			// otherwise push both
 			else if ( ! onlytrue )
 			{
 				VI.push_back(IA);
@@ -72,18 +191,27 @@ int lascomputetandem(libmaus2::util::ArgParser const & arg, libmaus2::util::ArgI
 			prevaread = OVL.aread;
 		}
 
-		VI = libmaus2::math::IntegerInterval<int64_t>::mergeTouchingOrOverlapping(VI);
-		for ( uint64_t i = 0; i < VI.size(); ++i )
-		{
-			libmaus2::util::NumberSerialisation::serialiseNumber(std::cout,prevaread);
-			libmaus2::util::NumberSerialisation::serialiseNumber(std::cout,VI[i].from);
-			libmaus2::util::NumberSerialisation::serialiseNumber(std::cout,VI[i].to);
-			//std::cerr << prevaread << "\t" << VI[i] << "\n";
-		}
-		VI.resize(0);
+		handle(VI,VT,Pdb,prevaread,verbose,bamfn);
 	}
 
+	VT = libmaus2::math::IntegerInterval<int64_t>::mergeTouchingOrOverlapping(VT);
+	for ( uint64_t i = 0; i < VT.size(); ++i )
+		std::cerr << "VT[" << i << "]=" << VT[i] << std::endl;
+
 	return EXIT_SUCCESS;
+}
+
+std::string getUsage(libmaus2::util::ArgParser const & arg)
+{
+	std::ostringstream ostr;
+
+	ostr << "usage: " << arg.progname << " [-d<reads.db> -b<reads.bam>] <in.las> ..." << std::endl;
+	ostr << "\n";
+	ostr << "parameters:\n";
+	ostr << " -d : Dazzler database for LAS files (default: none)\n";
+	ostr << " -b : BAM file storing ground truth alignments for reads (default: none)\n";
+
+	return ostr.str();
 }
 
 int main(int argc, char * argv[])
@@ -103,13 +231,12 @@ int main(int argc, char * argv[])
 			std::cerr << "This is " << PACKAGE_NAME << " version " << PACKAGE_VERSION << "." << std::endl;
 			std::cerr << PACKAGE_NAME << " is distributed under version 3 of the GPL." << std::endl;
 			std::cerr << "\n";
-			std::cerr << "usage: " << arg.progname << " [options] in.las\n";
+			std::cerr << getUsage(arg);
 			return EXIT_SUCCESS;
 		}
 		else
 		{
-			libmaus2::util::ArgInfo const arginfo(argc,argv);
-			return lascomputetandem(arg,arginfo);
+			return lascomputetandem(arg);
 		}
 	}
 	catch(std::exception const & ex)
