27dbd4d34cd29aca6932834ba7d20740c25287d4
German Tischler <tischler@mpi-cbg.de>
2017-11-09 20:15:51 +0100

    whitespace cleanup

--- src/lasshow.cpp
+++ src/lasshow.cpp
@@ -44,7 +44,7 @@ int64_t getDefaultTSpace()
 int lasshow(libmaus2::util::ArgParser const & arg)
 {
 	bool const showalignment = arg.uniqueArgPresent("a");
-	
+
 	std::string const db0name = arg[0];
 	std::string const db1name = arg[1];
 
@@ -52,7 +52,7 @@ int lasshow(libmaus2::util::ArgParser const & arg)
 	libmaus2::dazzler::db::DatabaseFile * pDB0 = 0;
 	libmaus2::dazzler::db::DatabaseFile::unique_ptr_type uDB1;
 	libmaus2::dazzler::db::DatabaseFile * pDB1 = 0;
-	
+
 	{
 		libmaus2::dazzler::db::DatabaseFile::unique_ptr_type tDB0(new libmaus2::dazzler::db::DatabaseFile(db0name));
 		uDB0 = UNIQUE_PTR_MOVE(tDB0);
@@ -61,7 +61,7 @@ int lasshow(libmaus2::util::ArgParser const & arg)
 
 	if ( db1name == db0name )
 	{
-		pDB1 = uDB0.get();		
+		pDB1 = uDB0.get();
 	}
 	else
 	{
@@ -79,7 +79,7 @@ int lasshow(libmaus2::util::ArgParser const & arg)
 	DB1.computeTrimVector();
 	std::vector<uint64_t> RL1;
 	DB1.getAllReadLengths(RL1);
-	
+
 	libmaus2::lcs::NP NP;
 	libmaus2::lcs::AlignmentTraceContainer ATC;
 
@@ -89,19 +89,21 @@ int lasshow(libmaus2::util::ArgParser const & arg)
 		int64_t const intspace = libmaus2::dazzler::align::AlignmentFile::getTSpace(arg[i]);
 		// int64_t const novl = libmaus2::dazzler::align::AlignmentFile::getNovl(arg[i]);
 		libmaus2::dazzler::align::AlignmentFileRegion::unique_ptr_type PIN(libmaus2::dazzler::align::OverlapIndexer::openAlignmentFileWithoutIndex(arg[i]));
-		
+
 		std::string a;
 		int64_t aid = -1;
 		std::string b;
 		std::string br;
 		int64_t bid = -1;
-		
+
 		while ( PIN->getNextOverlap(OVL) )
 		{
 			if ( OVL.aread != aid )
 			{
 				aid = OVL.aread;
 				a = DB0[aid];
+
+				//std::cerr << "[V] aid=" << aid << " length=" << a.size() << std::endl;
 			}
 			if ( OVL.bread != bid )
 			{
@@ -110,16 +112,16 @@ int lasshow(libmaus2::util::ArgParser const & arg)
 				br = libmaus2::fastx::reverseComplementUnmapped(b);
 			}
 
-			std::cout 
+			std::cout
 				<< aid << "[" << OVL.path.abpos << "," << OVL.path.aepos << ")/" << a.size()
-				<< " " 
-				<< bid 
-				<< (OVL.isInverse()?'c':'n') 
+				<< " "
+				<< bid
+				<< (OVL.isInverse()?'c':'n')
 				<< "[" << OVL.path.bbpos << "," << OVL.path.bepos << ")/" << b.size() << " " << OVL.path.diffs << " diffs " << OVL.getErrorSum() << " esum";
-			
+
 			if ( showalignment )
 			{
-				
+
 				uint8_t const * aptr = reinterpret_cast<uint8_t const *>(a.c_str());
 				uint8_t const * bptr =
 					OVL.isInverse()
@@ -127,12 +129,12 @@ int lasshow(libmaus2::util::ArgParser const & arg)
 					reinterpret_cast<uint8_t const *>(br.c_str())
 					:
 					reinterpret_cast<uint8_t const *>( b.c_str());
-				
+
 				OVL.computeTrace(aptr,bptr,intspace,ATC,NP);
 
 				std::cout << " " << ATC.getAlignmentStatistics() << "\n";
 				std::cout.put('\n');
-				
+
 				libmaus2::lcs::AlignmentPrint::printAlignmentLines(
 					std::cout,
 					aptr + OVL.path.abpos, OVL.path.aepos-OVL.path.abpos,
